83729477b08806ef97e727407e0db1f5e7b49cee
Ammar <ammar@gmil.com>
2026-09-14 11:20:00 +0300

    Edit and Delete added

--- PresentationLayer/Controllers/DepartmentController.cs
+++ PresentationLayer/Controllers/DepartmentController.cs
@@ -2,6 +2,7 @@
 using BuisnessAccessLayer.Repositories;
 using DataAccessLayer.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace PresentationLayer.Controllers
 {
@@ -34,15 +35,70 @@ namespace PresentationLayer.Controllers
             return View(department);
         }
 
-        public IActionResult Details(int? id)
+        public IActionResult Details(int? id, string view = "Details")
         {
             if (id == null)
                 return NotFound();
             var department = departmentRepository.Get(id);
             if (department == null)
                 return NotFound();
+            return View(view, department);
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            //if (id == null)
+            //    return NotFound();
+            //var department = departmentRepository.Get(id);
+            //if (department == null)
+            //    return NotFound();
+            //return View(department);
+
+            return Details(id, "Edit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int? id, Department department)
+        {
+            if (id != department.Id)
+                return BadRequest();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    departmentRepository.Update(department);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    return View(department);
+
+                }
+            }
             return View(department);
 
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            return Details(id, "Delete");
+        }
+
+        [HttpPost]
+        public IActionResult Delete([FromRoute] int? id, Department department)
+        {
+            if (id != department.Id)
+                return BadRequest();
+            try
+            {
+                departmentRepository.Delelte(department);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                return View(department);
+            }
         }
     }
 }
